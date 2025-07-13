9f7f41e951e4b1b06517f138f48cfdfc57c789fc
Alex <143654953+Cityships@users.noreply.github.com>
2025-07-13 10:29:38 -0500

    getting rid of some broken references

--- camera/camera_controller.gd
+++ camera/camera_controller.gd
@@ -4,4 +4,4 @@ extends Camera2D
 @export var follow_player_speed : float = 5.0
 
 func _physics_process(delta: float) -> void:
-    self.position = self.position.lerp(player.position + Vector2(0, -16), delta * follow_player_speed)
+	self.position = self.position.lerp(player.position + Vector2(0, -16), delta * follow_player_speed)
