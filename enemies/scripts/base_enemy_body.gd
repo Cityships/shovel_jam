91e4c7eabf6df12b738764d8fb9ef12ccada8a54
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-07-13 15:40:45 -0400

    Add base enemy scripts and test map scene
    Introduced BaseEnemyBody and BaseEnemyManager scripts for enemy movement and management logic. Added initial TestMap scene and enemy scene, updated default tileset with collision polygons, and set default texture filter in project settings.

---
+++ enemies/scripts/base_enemy_body.gd
@@ -0,0 +1,2 @@
+class_name BaseEnemyBody extends CharacterBody2D
+## Thinking of using this to hold Movement Logic. 
---
+++ enemies/scripts/base_enemy_manager.gd
@@ -0,0 +1,3 @@
+class_name BaseEnemyManager extends Node2D
+## Thinking of using this as the Main Observer for the Enemy Mobs.
+## All Signals from Enemy Body/Children will be receveid here. 
