e0a79d306f2f89e14a2522bfdfb3ffe92b4944c0
Alex <143654953+Cityships@users.noreply.github.com>
2025-07-14 15:16:24 -0500

    Emp now disables enemy.

--- enemy/scripts/base_enemy_manager.gd
+++ enemy/scripts/base_enemy_manager.gd
@@ -2,5 +2,5 @@ class_name BaseEnemyManager extends Node2D
 ## Thinking of using this as the Main Observer for the Enemy Mobs.
 ## All Signals from Enemy Body/Children will be receveid here. 
 @onready var base_enemy_body: BaseEnemyBody = %BaseEnemyBody
-@onready var sprite: Sprite2D = %Sprite
+@onready var sprite: Sprite2D = %VisualTreeRoot
 @onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
