94438cf8683b649241bdca9075966e21c4e1cfcd
Alex <143654953+Cityships@users.noreply.github.com>
2025-07-17 18:55:55 -0500

    Jetpack version 2 first commit

---
+++ scenes/level00/vent_door.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+@onready var area_2d : Area2D = get_node("Area2D")
+@onready var teleport_point = get_node("TeleportPoint")
+var jet_pack : Node2D
+
+func _ready() -> void:
+	area_2d.body_entered.connect(teleport_player)
+
+func teleport_player(player):
+	GlobalEvents.request_pickup_gadget.emit(get_tree().get_nodes_in_group("Jetpack")[0])
+	player.global_position = teleport_point.global_position
