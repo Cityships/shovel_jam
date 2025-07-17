8527720bcc0ba17cdc7cb98aafaf0918508abfe6
Alex <143654953+Cityships@users.noreply.github.com>
2025-07-16 19:56:09 -0500

    Adjusting level design

--- scenes/level00/doors/gadget_door/chargeable_door.gd
+++ scenes/level00/doors/gadget_door/chargeable_door.gd
@@ -5,6 +5,7 @@ signal emp_disabled
 @onready var path_follow : PathFollow2D = get_parent()
 @onready var gadget_door := get_parent().get_parent()
 @onready var windup_progress_bar : ProgressBar = get_node("ProgressBar")
+@export var animation_time : float = 3
 var door_opened = false
 
 func open_door():
@@ -12,8 +13,8 @@ func open_door():
 	tween.tween_method(
 		func(value):
 			path_follow.progress_ratio = value,
-		0.0, 1.0, 3
-	)
+		0.0, 1.0, animation_time
+	).set_trans(Tween.TRANS_QUAD)
 
 func recharge(value):
 	windup_progress_bar.value += value
