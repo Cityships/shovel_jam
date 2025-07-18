78fd3359dcd4d2f08f4fe295c10709b68d1e57db
Alex <143654953+Cityships@users.noreply.github.com>
2025-07-18 10:52:11 -0500

    Created electronic lock parameter, light key room finished, fixed a bug.

--- scenes/level00/doors/time_door/time_door_lock.gd
+++ scenes/level00/doors/time_door/time_door_lock.gd
@@ -1,10 +1,15 @@
 extends StaticBody2D
 
-@onready var open_door
+signal auto_trigger
+
 @onready var progress_bar :ProgressBar = get_node("ProgressBar")
 
+
 func recharge(amount_rotations : float):
-	progress_bar.value += amount_rotations	
+	progress_bar.value += amount_rotations
+	if progress_bar.value == progress_bar.max_value:
+		get_parent().door_locked = false
+		get_parent().open_door(0)
 
 func force_recharge(_amount_rotations : float):
 	recharge(_amount_rotations)
