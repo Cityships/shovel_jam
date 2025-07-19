5848182563eae22cf9bd8e2cea5bc50eebd877c9
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-07-19 09:13:00 -0400

    Fixed an error I made.

--- scenes/level00/items/light_key_item.gd
+++ scenes/level00/items/light_key_item.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
 @onready var area :Area2D = get_node("Area2D")
-
-
-    area.body_entered.connect(func(_value): Globals.obtained_keys.append(2); GlobalEvents.key_obtained.emit(); queue_free(),)
-
+func _ready() -> void:
+	area.body_entered.connect(func(_value): Globals.obtained_keys.append(2); GlobalEvents.key_obtained.emit(); queue_free(),)
