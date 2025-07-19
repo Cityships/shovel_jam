8d0ac90b720cda3e0da2f4528124d6e26cd54cab
Alex <143654953+Cityships@users.noreply.github.com>
2025-07-19 00:52:15 -0500

    Control display now response to progression. DO NOT TURN OFF.

---
+++ scenes/level00/checkpoint_area/emp_room_checkpoint.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var emp_node : Node2D
+
+func _ready() -> void:
+    body_entered.connect(
+        func(_value):
+            GlobalEvents.gadget_obtained.emit(emp_node),
+    )
